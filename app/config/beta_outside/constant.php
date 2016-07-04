db649f6861eba014cb6a8ecf1838df5d7c38d78c
Jade <jade@fivejack.com>
2016-07-04 19:19:25 +0700

    changed beta server domain

--- app/config/beta_outside/constant.php
+++ app/config/beta_outside/constant.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-	'BASE_URL' => 'http://fivejack.nexjack.com/',
-	'RESOURCE_URL' => 'http://fivejack.nexjack.com/'
+	'BASE_URL' => 'http://fivejack.gokil.me/',
+	'RESOURCE_URL' => 'http://fivejack.gokil.me/'
 );
